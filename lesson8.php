f3fea4b7fb11fd83a8ff69594e06a9b23a0c29d2
Aphihuk <ursocutest8@gmail.com>
2025-11-12 23:29:46 -0800

    Delete lesson8.php

--- lesson8.php
+++
@@ -1,7 +0,0 @@
-<?php
-// ສ້າງໄຟຂໍ້ມູນ HTML ໃນໄຟລ data.txt(ມັນຈະສ້າງauto ເວລາ run) ເເລະ ໄຊ້ຂໍມູນ
-file_put_contents("data.txt", "<p>HELLO <sub>WORLD</sub> </p>");
-// ອ່ານໄຟລ data.txt ແລະ ສະແດງຂໍ້ມູນ
-$file = file_get_contents("data.txt");
-echo $file;
-?>
